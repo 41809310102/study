5148c220b2cc0ee9e7e163286d4aca21c58faafd
41809310102 <2857154359@qq.com>
2020-11-24 19:31:52 +0800

    修改

--- 杨辉/杨辉/test.c
+++ 杨辉/杨辉/test.c
@@ -1,59 +1,38 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-void yanhui(int n)
-{
-	int arr[100][100] = {1};
-	int i,j;
-	for (i = 1; i <= n; i++)
+void yangHui(int n){
+	int arr[30][30] = { 1 }; //第一行直接填好，播下种子
+
+	int i, j;
+	for (i = 1; i < n; i++) //从第二行开始填
 	{
-		arr[i][0] = 1;
-		for (j = 1; j <= i; j++)
+	arr[i][0] = 1; //每行的第一列都没有区别，直接给1，保证不会越界。
+		for (j = 1; j <= i; j++) //从第二列开始填
 		{
 			arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
-
 		}
 	}
 
-	for (i = 0; i <= n; i++)
-	{ 
-		for (j = 0; j <= i; j++)
+
+	for (i = 0; i<n; i++)   //打印数组
+	{
+		for (j = 0; j<=i; j++)
 		{
-			printf("%d\n", arr[i][j]);
+			printf(" %d ", arr[i][j]);
 		}
 		putchar('\n');
 	}
+
 }
+
 
 
 int main(){
 	int a = 5;
-	yanhui(a);
+	yangHui(a);
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /*void yangHui(int n)
 {
@@ -82,9 +61,7 @@ int main(){
 
 
 int main(){
-	int a;
-	printf("输入你要打印的行数");
-	scanf("%d\n", &a);
+	int a=5;
 	yangHui(a);
 
 }*/
